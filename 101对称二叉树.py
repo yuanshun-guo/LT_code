bf6bec1b1412613471a0c927a75f6806c7e3de20
yuanshun-guo <guo_13797319692@163.com>
2021-07-01 18:32:50 +0800

    2021.7.1

---
+++ 101对称二叉树.py
@@ -0,0 +1,100 @@
+from collections import deque
+# 用collections.deque替换了原来的切片操作
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BiTreeHelper(object):
+    def __init__(self, nodeList = None):
+        self.nodeList = nodeList
+        self.root = self.generate()
+
+    def generate(self):
+        nodeList = deque(self.nodeList)
+        # nodeList == []
+        if not nodeList:  # 若列表为空
+            print("Empty nodeList!")
+            return None
+
+        # nodeList == [null, ...]
+        head_val = nodeList.popleft() # Remove and return the leftmost element.
+        if not head_val:  # 若左子节点的值为空
+            print("The value of head node is None!")
+            return None
+        vacancy = 2  # How many empty seats, if the value is even, \
+        # then the next node to be added must be the left node
+        root = TreeNode(head_val)
+        queue = deque([root])
+
+        while nodeList:
+            # Create current treenode
+            curr_val = nodeList.popleft()
+            if not curr_val and curr_val != 0:
+                curr = None
+            else:
+                curr = TreeNode(curr_val)
+                queue.append(curr)
+                vacancy = vacancy + 2
+
+            # Link to its parent
+            parent = queue[0]
+            if not parent.left and vacancy % 2 == 0:
+                # if parent.left is available
+                parent.left = curr
+            else:
+                parent.right = curr
+                queue.popleft()
+            vacancy = vacancy - 1
+        return root
+
+    def inorder_traversal_print(self, root=None): # 显示了取值的过程
+        print("BiTree inorder traversal print:")
+        if not root:
+            root = self.root
+        self.dfs(root)
+
+    def dfs(self, root):
+        if root:
+            self.dfs(root.left)
+            print(root.val)
+            self.dfs(root.right)
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+
+# nl1 = [3,5,1,6,2,9,8,None,None,7,4,6]
+nl2 = [1,2,2,'null',3,'null',3]
+
+# bi1 = BiTreeHelper(nl1)
+# bi1.inorder_traversal_print()
+bi2 = BiTreeHelper(nl2)
+bi2.inorder_traversal_print()
+
+s = Solution()
+print(s.inorderTraversal(bi2.root))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ f_x/19冒泡排序.py
@@ -0,0 +1,11 @@
+def bubbleSort(list0):
+    for name in range(len(list0) - 1, 0, -1):
+        for i in range(name):
+            if list0[i] > list0[i + 1]:
+                list0[i + 1], list0[i] = list0[i], list0[i + 1]
+            else:
+                pass
+    return list0
+
+list0 = [5, 6, 2, 8, 4]
+print(bubbleSort(list0))
