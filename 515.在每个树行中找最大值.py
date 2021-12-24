2c24f1cc068b543a885456f5aebcade9ea53d6e5
yuanshun-guo <guo_13797319692@163.com>
2021-12-24 19:49:56 +0800

    2021.12.24

---
+++ 515.在每个树行中找最大值.py
@@ -0,0 +1,42 @@
+"""
+前提：层序遍历 就是 广度优先搜索，就是一层一层的遍历，需借助队列来实现
+"""
+
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    """二叉树层序遍历迭代解法"""
+
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        from collections import deque  # 导入第三方模块也可以在实现的代码中
+        deque = deque([root])
+        results = []  # 这个是最终结果
+
+        while deque:
+            size = len(deque)
+            result = []    # 因为要每一层读，所以需要按时清空
+            while size:  # 实现每一层的读取
+                cur = deque.popleft()
+                result.append(cur.val)
+                if cur.left:
+                    deque.append(cur.left)
+                if cur.right:
+                    deque.append(cur.right)
+
+                size -= 1
+            results.append(result)
+
+        return results
+
+
+
